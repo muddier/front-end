26c5f5e2d2a8484f0ac0c58fb415fb72a162927b
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2020-02-05 11:46:06 -0600

    initial setup

--- src/components/game/NavBar.js
+++ src/components/game/NavBar.js
@@ -1,6 +1,19 @@
 import React from "react";
-
+import Pusher from 'pusher-js';
 function NavBar(props) {
+
+  Pusher.logToConsole = true;
+
+  const pusher = new Pusher('0eed8913739ddced563b', {
+    cluster: 'us2',
+    forceTLS: true
+  });
+
+  let channel = pusher.subscribe('room-{}');
+  channel.bind('broadcast', data => {
+  alert(JSON.stringify(data));
+  });
+
   return (
     <div
       className=""
