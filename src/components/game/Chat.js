3c3726970bb8ded2e34d46c4ff378501cf68ea01
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2020-02-06 15:14:53 -0600

    added room name

--- src/components/game/Chat.js
+++ src/components/game/Chat.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Pusher from 'pusher-js';
 import { Button, Form } from "react-bootstrap";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
-import './Chat.css';
+import '../styles/Chat.css';
 function Chat(props) {
     const [ message, setMessage ] = useState('')
     const [ chat, setChat ] = useState([])
@@ -20,6 +20,7 @@ function Chat(props) {
         channel.bind('broadcast', data => {
             setChat(oldChat => [...oldChat, data])
         });
+        
     }, [props.roomId])
 
     const handleSubmit = (e) => {
@@ -38,11 +39,14 @@ function Chat(props) {
     const handleChange = (e) => {
         setMessage(e.target.value)
     }
-
+console.log("Room data", props.charactersData)
     return (
         <div className="chatbox"  >
+            <div className="screen room-name">
+                <h5 style={{textAlign:"center", verticalAlign: "center", textTransform: "capitalize", fontSize: "1.8rem"}}>Location: {props.charactersData.title}</h5>
+            </div>
             <Form onSubmit={handleSubmit} style={{ height: "100%", display: "flex", flexDirection: "column", margin: "10px"}}>
-                <h5 className="chat-display">
+                <h5 className="chat-display screen">
                     <div style={{ transformOrigin: "50% 50%", transform: "rotate(180deg)", scrollBehavior:"reverseScroll()"}}>
                     {chat.length !== 0 && chat.map((msg, i) => {
                         return <p key={i}><span style={{ color: "silver " }}>>>> {msg.user}: </span> {msg.message} </p>
@@ -51,14 +55,14 @@ function Chat(props) {
                 </h5>
                  
                 <input
-                    style={{ fontFamily: "VT323", fontSize: "1.3rem" }}
+                    style={{ fontFamily: "VT323", fontSize: "1.3rem", margin: "10px" }}
                     name="chat"
                     value={message}
                     onChange={handleChange}
                     type="text"
                     placeholder=">>> What's the buzz?"
                 />
-                <Button style={{backgroundColor: "purple", margin: "1rem 0" }}className="send-btn" type="submit">Send</Button>
+                <Button style={{backgroundColor: "purple", margin: "10px", maxWidth:"300px"}}className="send-btn" type="submit">Send</Button>
             </Form> 
         </div>
 
