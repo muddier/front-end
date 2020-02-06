0d2f05aff8a4ae541ea2120d43670fbc1c3d997e
DM <dm@DMs-MacBook-Pro.fios-router.home>
2020-02-06 16:34:03 -0500

    styled leaderboard, added to nav

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -2,62 +2,94 @@ import React from "react";
 import { NavLink, withRouter } from "react-router-dom";
 
 function MainNavigation(props) {
-  const handleLogOut = () => {
-    localStorage.removeItem("key");
-    localStorage.removeItem("pusherTransportTLS");
-    props.history.push("/");
-  };
-  return (
-    <nav style={{ display: "flex", justifyContent: "flex-end" }}>
-      {localStorage.getItem("key") ? (
-        <p
-          onClick={handleLogOut}
-          style={{
-            cursor: "pointer",
-            color: "silver",
-            fontSize: "14px",
-            margin: "8px",
-            border: "1px solid silver",
-            padding: "6px 10px",
-            borderRadius: "10px"
-          }}
-        >
-          sign out
-        </p>
-      ) : (
-        <>
-          <NavLink
-            style={{
-              fontSize: "14px",
-              margin: "8px",
-              color: "#f8c129",
-              border: "1px solid #f8c129",
-              padding: "8px",
-              borderRadius: "10px",
-              textDecoration: "none"
-            }}
-            to="/signin"
-          >
-            Sign In
-          </NavLink>{" "}
-          <NavLink
-            style={{
-              fontSize: "14px",
-              margin: "8px",
-              background: "#F89500",
-              color: "#1d1d1d",
-              padding: "9px",
-              borderRadius: "10px",
-              textDecoration: "none"
-            }}
-            to="/signup"
-          >
-            Create Account
-          </NavLink>
-        </>
-      )}
-    </nav>
-  );
+	const handleLogOut = () => {
+		localStorage.removeItem("key");
+		localStorage.removeItem("pusherTransportTLS");
+		props.history.push("/");
+	};
+	return (
+		<nav style={{ display: "flex", justifyContent: "flex-end" }}>
+			{localStorage.getItem("key") ? (
+				<>
+					<NavLink
+						style={{
+							fontSize: "14px",
+							margin: "8px",
+							background: "#F89500",
+							color: "#1d1d1d",
+							padding: "9px",
+							borderRadius: "10px",
+							textDecoration: "none",
+						}}
+						className='navlink'
+						to='/game'
+					>
+						home
+					</NavLink>
+					<NavLink
+						style={{
+							fontSize: "14px",
+							margin: "8px",
+							background: "#F89500",
+							color: "#1d1d1d",
+							padding: "9px",
+							borderRadius: "10px",
+							textDecoration: "none",
+						}}
+						className='navlink'
+						to='/leaderboard'
+					>
+						leaderboard
+					</NavLink>
+					<p
+						onClick={handleLogOut}
+						style={{
+							cursor: "pointer",
+							color: "silver",
+							fontSize: "14px",
+							margin: "8px",
+							border: "1px solid silver",
+							padding: "6px 10px",
+							borderRadius: "10px",
+						}}
+					>
+						sign out
+					</p>
+				</>
+			) : (
+				<>
+					<NavLink
+						style={{
+							fontSize: "14px",
+							margin: "8px",
+							color: "#f8c129",
+							border: "1px solid #f8c129",
+							padding: "8px",
+							borderRadius: "10px",
+							textDecoration: "none",
+						}}
+						to='/signin'
+					>
+						Sign In
+					</NavLink>{" "}
+					<NavLink
+						style={{
+							fontSize: "14px",
+							margin: "8px",
+							background: "#F89500",
+							color: "#1d1d1d",
+							padding: "9px",
+							borderRadius: "10px",
+							textDecoration: "none",
+						}}
+						to='/signup'
+					>
+						Create Account
+					</NavLink>
+				</>
+			)}
+		</nav>
+	);
 }
 
 export default withRouter(MainNavigation);
--- src/components/game/Leaderboard.js
+++ src/components/game/Leaderboard.js
@@ -4,9 +4,28 @@ import axios from "axios";
 function LeaderList({ leader }) {
 	console.log(leader);
 	return (
-		<div>
-			<strong>{leader.username} </strong>
-			<span>Honey: {leader.honey}</span> <span>XP: {leader.xp}</span>
+		<div
+			style={{
+				borderBottom: "1px solid purple",
+				display: "grid",
+				gridTemplateColumns: "50% auto auto",
+				width: "650px",
+				margin: "0 auto",
+				padding: "8px 0",
+				fontFamily: "Major Mono Display",
+			}}
+		>
+			<span
+				style={{
+					textAlign: "left",
+					marginLeft: "15px",
+					fontWeight: "bold",
+				}}
+			>
+				{leader.username}
+			</span>
+			<span style={{ textAlign: "center" }}>{leader.honey}</span>
+			<span style={{ textAlign: "center" }}>{leader.xp}</span>
 		</div>
 	);
 }
@@ -28,7 +47,25 @@ function Leaderboard() {
 
 	return (
 		<div>
-			<h1>Leader Board</h1>
+			<div
+				style={{
+					display: "grid",
+					gridTemplateColumns: "50% auto auto",
+					width: "650px",
+					margin: "0 auto",
+					padding: "5px 0",
+					fontWeight: "bold",
+					fontSize: "20px",
+					color: "purple",
+					background: "black",
+				}}
+			>
+				<span style={{ textAlign: "left", marginLeft: "15px" }}>
+					USER
+				</span>
+				<span style={{ textAlign: "center" }}>HONEY</span>
+				<span style={{ textAlign: "center" }}>XP</span>
+			</div>
 			{leaders.map((leader, index) => {
 				return <LeaderList leader={leader} key={index} />;
 			})}
