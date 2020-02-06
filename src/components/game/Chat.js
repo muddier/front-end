0aa7aa6b7e21aa20f2cde0e27faba96d618f8704
Lucas Bazemore <lucasabazemore@gmail.com>
2020-02-06 14:38:26 -0600

    fixed pusher and cleaned up some styling

--- src/components/game/Chat.js
+++ src/components/game/Chat.js
@@ -1,26 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import Pusher from 'pusher-js';
+import React, { useState, useEffect, useRef } from 'react';
 import { Button, Form } from "react-bootstrap";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
+import PusherSubscription from './Pusher';
 import './Chat.css';
-function Chat(props) {
-    const [ message, setMessage ] = useState('')
-    const [ chat, setChat ] = useState([])
-    useEffect(() => {        
-        setChat([])
-        Pusher.logToConsole = true;
-        
-        const pusher = new Pusher('2dad5e9a4335ac2f740f', {
-          cluster: 'us2',
-          forceTLS: true
-        });
-      
-        let channel = pusher.subscribe(`room-${props.roomId}`);
-        
-        channel.bind('broadcast', data => {
-            setChat(oldChat => [...oldChat, data])
-        });
-    }, [props.roomId])
+
+const Message = () => {
+    const [message, setMessage] = useState('')
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -40,26 +25,38 @@ function Chat(props) {
     }
 
     return (
-        <div className="chatbox"  >
-            <Form onSubmit={handleSubmit} style={{ height: "100%", display: "flex", flexDirection: "column", margin: "10px"}}>
-                <h5 className="chat-display">
-                    <div style={{ transformOrigin: "50% 50%", transform: "rotate(180deg)", scrollBehavior:"reverseScroll()"}}>
-                    {chat.length !== 0 && chat.map((msg, i) => {
-                        return <p key={i}><span style={{ color: "silver " }}>>>> {msg.user}: </span> {msg.message} </p>
-                    })}
-                    </div> 
-                </h5>
-                 
-                <input
-                    style={{ fontFamily: "VT323", fontSize: "1.3rem" }}
-                    name="chat"
-                    value={message}
-                    onChange={handleChange}
-                    type="text"
-                    placeholder=">>> What's the buzz?"
-                />
-                <Button style={{backgroundColor: "purple", margin: "1rem 0" }}className="send-btn" type="submit">Send</Button>
-            </Form> 
+        <Form onSubmit={handleSubmit} style={{ height: "100%", display: "flex", flexDirection: "column", margin: "0px"}}>
+        <input
+            style={{ fontFamily: "VT323", fontSize: "1.3rem" }}
+            name="chat"
+            value={message}
+            onChange={handleChange}
+            type="text"
+            placeholder=">>> What's the buzz?"
+        />
+        <Button style={{backgroundColor: "purple", margin: "1rem 0", border: "none" }}className="send-btn" type="submit">Send</Button>
+    </Form> 
+    )
+}
+
+function Chat({roomId}) {
+    const [ chat, setChat ] = useState([])
+
+    useEffect(() => {        
+        setChat([])
+    }, [roomId])
+
+    return (
+        <div className="chat-box">
+            <PusherSubscription roomId={roomId} setChat={setChat} />
+            <h5 className="chat-display">
+                <div style={{ transformOrigin: "50% 50%", transform: "rotate(180deg)", scrollBehavior:"reverseScroll()"}}>
+                {chat.length !== 0 && chat.map((msg, i) => {
+                    return <p key={i}><span style={{ color: "silver " }}>>>> {msg.user}: </span> {msg.message} </p>
+                })}
+                </div> 
+            </h5>
+            <Message />
         </div>
 
     )
--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -2,14 +2,11 @@ import React, { useState, useEffect, useRef } from "react";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
 import World from "./World";
 import Controls from "./Controls";
-import NavBar from "./NavBar";
 import SideBar from "./SideBar";
 import Chat from './Chat';
 
 function Game() {
   const [currentRoom, setCurrentRoom] = useState({});
-  const [moveErrMsg, setMoveErrMsg] = useState("");
-  const [nextRooms, setNextRooms] = useState([]);
 
   useEffect(() => {
     // set the player in the intial room
@@ -19,7 +16,6 @@ function Game() {
       .then(res => {
         console.log("Response: ", res);
         setCurrentRoom(res.data);
-        setNextRooms(res.data.nextRooms);
       })
       .catch(err => {
         console.log(err.response);
@@ -38,8 +34,6 @@ function Game() {
       .then(res => {
         console.log(res);
         setCurrentRoom(res.data);
-        setMoveErrMsg(res.data.error_msg);
-        setNextRooms(res.data.nextRooms);
       })
       .catch(err => {
         console.log(err.response);
@@ -49,50 +43,49 @@ function Game() {
   if (!currentRoom.players) return <h1>Loading...</h1>;
   return (
     <main style={{ display: "flex", margin: "auto 0", justifyContent: "center" }}>
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        marginRight: "1rem"
-      }}
-    >
-
       <div
         style={{
           display: "flex",
-          flexDirection: "row-reverse",
-          justifyContent: "center",
-          margin: "10px",
-          border: "1px solid silver",
-          borderRadius: "10px"
+          flexDirection: "column",
+          alignItems: "center",
+          marginRight: "1rem"
         }}
       >
+
         <div
           style={{
             display: "flex",
-            flexDirection: "column",
-            justifyContent: "space-between",
-            background: "#000",
-            width: '200px',
-            borderRadius: "0 10px 10px 0"
+            flexDirection: "row-reverse",
+            justifyContent: "center",
+            margin: "10px",
+            border: "1px solid silver",
+            borderRadius: "10px"
           }}
         >
-          <SideBar currentRoom={currentRoom} />
-          <Controls
-            moveRooms={moveRooms}
-            nextRooms={nextRooms}
-            moveErrMsg={moveErrMsg}
-          />
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-between",
+              background: "#000",
+              width: '200px',
+              borderRadius: "0 10px 10px 0"
+            }}
+          >
+            <SideBar currentRoom={currentRoom} />
+            <Controls
+              moveRooms={moveRooms}
+              nextRooms={currentRoom.nextRooms}
+              moveErrMsg={currentRoom.error_msg}
+            />
+          </div>
+          <World currentRoom={currentRoom}/>
         </div>
-        <World currentRoom={currentRoom}/>
+        
       </div>
-      <NavBar currentRoom={currentRoom} />
-      
-    </div>
-    <Chat roomId={currentRoom.roomId}/>
-    </main>
-        );
+      <Chat roomId={currentRoom.roomId}/>
+      </main>
+  );
 }
 
 export default Game;
