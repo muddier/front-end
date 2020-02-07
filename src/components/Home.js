06e840e8e9d6485c9bfa5ba402f2e097615e439b
DM <dm@DMs-MacBook-Pro.fios-router.home>
2020-02-07 01:48:42 -0500

    styles for landing page hero, replaced space ipsum

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,68 +5,95 @@ import bear from "../assets/spacesuit_bear.png";
 import "../styles/home.scss";
 
 function Home() {
-  return (
-    <>
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "60% 40%",
-          height: "400px",
-          width: "80%",
-          border: "solid 1px silver",
-          borderRadius: "10px",
-          padding: "25px",
-          margin: "30px auto 50px auto",
-          backgroundColor: "black",
-          color: "#99E550"
-        }}
-      >
-        <div className="animation-ctr">
-          <img src={bear} alt="spacebear" className="spacebear" height="60" />
-          <figure id="bees">
-            <img src={bee} alt="bee" />
-            <img src={bee} alt="bee" />
-          </figure>
-        </div>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center"
-          }}
-        >
-          <h2>Welcome to Space Beez!</h2>
-          <p>
-            Space ipsum it suddenly struck me that that tiny pea, pretty and
-            blue, was the Earth. I didn’t feel like a giant. I felt very, very
-            small.
-          </p>
-          <p>
-            Space ipsum it suddenly struck me that that tiny pea, pretty and
-            blue, was the Earth. I put up my thumb and shut one eye, and my
-            thumb blotted out the planet Earth.
-          </p>
-          <NavLink
-            style={{
-              fontSize: "24px",
-              textDecoration: "none",
-              color: "purple",
-              margin: "20px auto",
-              fontWeight: "bold",
-              border: "1px solid silver",
-              borderRadius: "10px",
-              background: "black",
-              padding: "15px 100px"
-            }}
-            to="/signin"
-          >
-            Play Now!
-          </NavLink>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div
+				style={{
+					display: "grid",
+					gridTemplateColumns: "50% 50%",
+					height: "650px",
+					width: "95%",
+					border: "solid 5px #778678",
+					borderRadius: "10px",
+					padding: "25px",
+					margin: "30px auto 50px auto",
+					backgroundColor: "black",
+					backgroundImage:
+						"url(https://images.unsplash.com/photo-1520034475321-cbe63696469a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9)",
+					backgroundSize: "contain",
+					maxWidth: "1225px",
+				}}
+			>
+				<div className='animation-ctr'>
+					<img
+						src={bear}
+						alt='spacebear'
+						className='spacebear'
+						height='60'
+					/>
+					<figure id='bees'>
+						<img src={bee} alt='bee' />
+						<img src={bee} alt='bee' />
+					</figure>
+				</div>
+				<div
+					style={{
+						display: "flex",
+						flexDirection: "column",
+						alignItems: "center",
+						justifyContent: "center",
+						fontFamily: "VT323",
+						fontSize: "1.3rem",
+						fontWeight: "bold",
+						maxWidth: "475px",
+						textAlign: "justify",
+						paddingTop: "5px",
+					}}
+				>
+					<p>
+						It's the year 5051. Centuries ago, your Space Bear
+						ancestors became the great rulers of the universe after
+						bringing the evil Homo Sapiens' legacy of destruction to
+						an end.
+					</p>
+					<p>
+						Your society is now teetering on the brink of collapse
+						due to a recent shortage of your planet's main source of
+						fuel: Honey.
+					</p>
+					<p>
+						Our only hope is to travel across the galaxy to the last
+						remaining hive, filled with dangerous, killer Space Beez
+						who will take your life to protect their precious
+						nectar.
+					</p>
+					<p>
+						You have been selected as one of the most elite Space
+						Bears with defeating the evil Space Beez and taking all
+						the honey that's left before they rise up and take over
+						the universe.
+					</p>
+					<NavLink
+						style={{
+							fontSize: "38px",
+							textDecoration: "none",
+							color: "purple",
+							margin: "0 auto",
+							fontWeight: "bold",
+							border: "5px solid purple",
+							borderRadius: "10px",
+							background: "black",
+							padding: "5px 10px",
+							textTransform: "uppercase",
+						}}
+						to='/signin'
+					>
+						Accept Mission
+					</NavLink>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default Home;
